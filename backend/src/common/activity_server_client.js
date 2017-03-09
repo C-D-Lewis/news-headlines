48c3603e853f53c58c009048d19473fd0d6b0eb0
C-D-Lewis <Bonsitm@gmail.com>
2017-03-09 21:38:00 +0000

    Sync common module

--- backend/src/common/activity_server_client.js
+++ backend/src/common/activity_server_client.js
@@ -1,3 +1,5 @@
+// Requires Monitor instance on same box with ACTIVITY_SERVER enabled
+
 var request = require('request');
 
 var config = require('../../config.json');
@@ -5,7 +7,7 @@ var log = require('./log.js');
 
 function post() {
   if(!config.ACTIVITY_SERVER.POST) return;
-  
+
   var url = 'http://127.0.0.1:' + config.ACTIVITY_SERVER.PORT + '/blink';
   request.post(url, function(err, response, body) {
     if(err) {
